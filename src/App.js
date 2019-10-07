510ca8e335dfa2324a42afc9f7b729a694ae6cc0
Cody Luth <luthcody@gmail.com>
2019-10-06 18:33:32 -0700

    Added layout section - beginning header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './navigation/Header';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
