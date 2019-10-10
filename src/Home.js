5efba1d5d297dbc02f5902a88eabc19eafa1ab9c
Cody Luth <luthcody@gmail.com>
2019-10-09 22:01:45 -0700

    Added Text

--- src/Home.js
+++ src/Home.js
@@ -9,8 +9,8 @@ function Home() {
       <div className="hero-banner">
         <img src={zebra} alt="Zebra" width="100%"/>
       </div>
+      <h1>This site is under construction.</h1>
     </div>
-
   );
 }
 
